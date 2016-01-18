b1d199a288d73ebe360f77238173db955175dff7
Student <student@RU-007.local>
2016-01-18 11:19:40 -0700

    all done

---
+++ js2.js
@@ -0,0 +1,13 @@
+var prompt = prompt("Enter any word!")
+
+alert("The lenght of your word is " + prompt.length)
+
+alert("The 3rd character is " + prompt.charAt(2))
+
+alert("Your word in all uppercase is " + prompt.toUpperCase())
+
+alert("Your word in all lowercase is " + prompt.toLowerCase())
+
+alert(prompt + " is a delicious fruit!")
+
+alert("The subword is " + prompt.substring(1, 4))
